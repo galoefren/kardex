26f20991a9e3308964a0dc03cc9efa7bd08da1dd
galoefren <35548025+galoefren@users.noreply.github.com>
2021-02-11 23:23:04 -0500

    rm

--- web/src/main/java/recursos/Constantes.java
+++
@@ -1,8 +0,0 @@
-package recursos;
-
-public class Constantes {
-
-	public static final String NO_EXISTE_USUARIO = "NO EXISTE USUARIO";
-	public static final String PASSWORD_INVALIDO = "PASSWORD INVALIDO";
-	
-}
